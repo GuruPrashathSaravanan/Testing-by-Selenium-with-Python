4b1cb8763573bdd96494a2708b0ce52e4a5d9e23
Guru prashath S <sachinprashath@gmail.com>
2024-10-27 12:16:02 +0530

    moving all files to repo

---
+++ first_selenium_script.py
@@ -0,0 +1,20 @@
+# Import Selenium WebDriver
+from selenium import webdriver
+
+# Specify the path to the ChromeDriver
+# If ChromeDriver is not in your system PATH, provide the exact path to the driver
+
+url = "https://www.google.com"
+url2= 'https://www.amazon.com'
+
+# Open Google
+driver = webdriver.Chrome()
+driver.get(url)
+
+# Print the title of the current webpage
+print("Page Title is: ", driver.title)
+
+# Close the browser
+driver2 = webdriver.Chrome()
+driver2.get(url2)
+print("the web site name :",driver2.title)
