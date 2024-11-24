5e2b3e58a78f2c39d8f1313a42a1612bce5c1787
Guru prashath S <sachinprashath@gmail.com>
2024-11-24 10:34:59 +0530

    nagivation

--- first_selenium_script.py
+++ first_selenium_script.py
@@ -4,18 +4,24 @@ from selenium import webdriver
 # Specify the path to the ChromeDriver
 # If ChromeDriver is not in your system PATH, provide the exact path to the driver
 
-url = "https://www.google.com"
+url1= "https://www.google.com"
 url2= 'https://www.amazon.com'
 
 # Open Google
-driver = webdriver.Chrome()
-driver.get(url)
+driver=webdriver.Chrome()
+driver.get(url1)
+driver.back()
+driver.get(url2)
+driver.back()
+driver.implicitly_wait(100)
+driver.forward()
 
 # Print the title of the current webpage
 print("Page Title is: ", driver.title)
 
-# Close the browser
+""" # Close the browser
 driver2 = webdriver.Chrome()
 driver2.get(url2)
+
 print("the web site name :",driver2.title)
-print("the name of the user is guru")
+print("the name of the user is guru") """
